b1e07eadb5095cee60027dd57dfe930918f96399
unknown <ksantillana1400@alumno.ipn.mx>
2020-04-02 17:35:47 -0600

    Modification on the collection preview component

--- src/components/collection-preview/collection-preview-component.jsx
+++ src/components/collection-preview/collection-preview-component.jsx
@@ -1,12 +1,14 @@
  import React from 'react';
- import './collection-preview.styles.scss';
+ import './collection-preview-component.styles.scss';
 
  const CollectionPreview = ({title, items}) => (
      <div className='collection-preview'>
          <h1 className='title'>{title.toUpperCase()}</h1>
          <div className='preview'>
             {
-                items.map((item) => (
+                items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
                     <div key={item.id}>{item.name}</div>
                 ))
             }
