7e80cf83c54c97b22764346aa193ea3400487802
Katia Santillán <ksantillana1400@alumno.ipn.mx>
2020-09-15 13:45:27 -0500

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included withSpinner HOC

--- src/components/collection-item/collection-item-component.jsx
+++ src/components/collection-item/collection-item-component.jsx
@@ -7,26 +7,36 @@ import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item-component.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
-    const {name, price, imageUrl } = item;
-    return(
-        <div className='collection-item'>
-            <div 
-                className='image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
+    const { name, price, imageUrl } = item;
+  
+    return (
+      <div className='collection-item'>
+        <div
+          className='image'
+          style={{
+            backgroundImage: `url(${imageUrl})`
+          }}
+        />
+        <div className='collection-footer'>
+          <span className='name'>{name}</span>
+          <span className='price'>{price}</span>
         </div>
+        <CustomButton
+          className='custom-button'
+          onClick={() => addItem(item)}
+          inverted
+        >
+          Add to cart
+        </CustomButton>
+      </div>
     );
-};
-
-const mapDispatchToProps = dispatch => ({
+  };
+  
+  const mapDispatchToProps = dispatch => ({
     addItem: item => dispatch(addItem(item))
-})
-
-export default connect(null, mapDispatchToProps)(CollectionItem);
+  });
+  
+  export default connect(
+    null,
+    mapDispatchToProps
+  )(CollectionItem);
